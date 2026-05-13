85b60ebeca4ece6599fec873d2b38f963522f417
montse-jara <jaramontse3@gmail.com>
2026-05-13 14:54:01 -0700

    Update comments for localization.launch.py

--- src/rover_bringup/launch/localization.launch.py
+++ src/rover_bringup/launch/localization.launch.py
@@ -1,11 +1,26 @@
+"""Launches nodes for Localization
+
+This module launches nodes for Nav2 
+plugins responsible for setting intial pose
+and localizing during navigation.Before launching
+this file the user must source the environment and
+launch odom_bring up. After launching this file an 
+intial pose must be set either manually via the terminal
+or by setting the pose using the gui tool Rviz.
+
+usage:
+    ros2 launch rover_bringup localization.launch.py
+"""
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
-
+# this function launches map server, acml, and lifecycle manager
 def generate_launch_description():
     bringup_dir = get_package_share_directory('rover_bringup')
+    # expected behavior of plugins is defined in the param file
     nav2_params = os.path.join(bringup_dir, 'config', 'nav2_params.yaml')
 
     return LaunchDescription([
